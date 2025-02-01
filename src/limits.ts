b39a95b2d23e4a391aafc59ea266a24210ab7ec0
Yuchen Chang <phiricen+i@gmail.com>
2025-02-01 15:28:20 +0900

    v1.0.1.10

--- src/limits.ts
+++ src/limits.ts
@@ -1,5 +1,6 @@
 export class TokenLimits {
   maxTokens: number
+  maxCompletionTokens?: number
   requestTokens: number
   responseTokens: number
   knowledgeCutOff: string
@@ -23,18 +24,34 @@ export class TokenLimits {
       this.responseTokens = 2000
     } 
     else if (model === 'o3-mini') {
-      this.maxTokens = 200000;
+      this.maxTokens = 200000
+      this.maxCompletionTokens = 200000
       this.responseTokens = 100000;
     } 
     else {
       this.maxTokens = 4000
       this.responseTokens = 1000
     }
+
     // provide some margin for the request tokens
-    this.requestTokens = this.maxTokens - this.responseTokens - 100
+    if (this.maxCompletionTokens){
+      this.requestTokens = this.maxCompletionTokens - this.responseTokens - 100
+    }
+    else if (this.maxTokens) {
+      this.requestTokens = this.maxTokens - this.responseTokens - 100
+    }
+    else {
+      this.requestTokens = 4000
+    }
   }
 
   string(): string {
-    return `max_tokens=${this.maxTokens}, request_tokens=${this.requestTokens}, response_tokens=${this.responseTokens}`
+    return `
+      max_tokens=${this.maxTokens}, 
+      request_tokens=${this.requestTokens}, 
+      response_tokens=${this.responseTokens}, 
+      max_completion_tokens=${this.maxCompletionTokens}, 
+      knowledgeCutOff=${this.knowledgeCutOff}
+    `
   }
 }
